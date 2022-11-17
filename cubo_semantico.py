e20c10e447f84c58a086e84865a7ee37a5d9fe92
nadia.co <ngarciaorozco@outlook.com>
2022-11-17 01:16:57 -0600

    creacion de cubo semantico

---
+++ cubo_semantico.py
@@ -0,0 +1,314 @@
+cuboSemantico = {
+    "+" : {
+        "int": {
+            "int" : "int",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "string"
+        }, 
+        "float" : {
+            "int" : "float",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "string"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "string"
+        }, 
+        "string": {
+            "int" : "string",
+            "float": "string",
+            "bool" : "string",
+            "string" : "string"
+        }
+    },
+    "-" : {
+        "int": {
+            "int" : "int",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "float",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "*" : {
+        "int": {
+            "int" : "int",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "float",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "/" : {
+        "int": {
+            "int" : "int",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "float",
+            "float": "float",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "==" : {
+        "int": {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "bool",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "bool"
+        }
+    },
+    "=!" : {
+        "int": {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "bool",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "bool"
+        }
+    },
+    ">" : {
+        "int": {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "<" : {
+        "int": {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    ">=" : {
+        "int": {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "<=" : {
+        "int": {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "bool",
+            "float": "bool",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "and" : {
+        "int": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "bool",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    },
+    "or" : {
+        "int": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }, 
+        "float" : {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        },
+        "bool": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "bool",
+            "string" : "ERROR"
+        }, 
+        "string": {
+            "int" : "ERROR",
+            "float": "ERROR",
+            "bool" : "ERROR",
+            "string" : "ERROR"
+        }
+    }
+}
